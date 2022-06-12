06ad68e0c0424b828efc5d14bd061fe28cb5ebb1
betrayalove <Android620072@yandex.ru>
2022-06-12 19:48:10 +0500

    add allowed hosts

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-jkr^orxs*4=ks#
 # DEBUG = False
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['tochka-auction-platform.herokuapp.com']
+ALLOWED_HOSTS = ['tochka-auction-platform.herokuapp.com', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://tochka-auction-platform.herokuapp.com']
 
 # Application definition
